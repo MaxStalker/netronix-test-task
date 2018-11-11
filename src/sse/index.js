5fade3d1b1c254bb6d74dc9917429f12d5faaa45
max <max.daunarovich@gmail.com>
2018-11-12 01:57:10 +0300

    Create helper class for handling EventSource

--- src/sse/index.js
+++ src/sse/index.js
@@ -1,12 +1,21 @@
-let eventSource;
+class EventSourceConnection {
+  constructor(url, processor) {
+    console.log('Establish connection');
+    this.connection = new EventSource(url);
+    if (processor) {
+      this.bindProcessor(processor);
+    }
+  }
 
-export const setupConnection = () => {
-  eventSource = new EventSource('https://jsdemo.envdev.io/sse');
-  eventSource.onmessage = function(e) {
-    // Send actions here
-  };
-};
+  bindProcessor(processor) {
+    console.log('Assign processor');
+    this.connection.onmessage = processor;
+  }
 
-export const closeConnection = () => {
-  eventSource.close();
-};
+  closeConnection() {
+		console.log('Connection closed');
+    this.closeConnection();
+  }
+}
+
+export default EventSourceConnection;
